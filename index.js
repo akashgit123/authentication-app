85d9bf2d066d7c6c518f657399d338833d5f278d
Akash <akashshettigar709@gmail.com>
2023-12-19 10:35:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
+const port = process.env.PORT;
+
+app.get('/',(req,res)=>{
+    res.send("Worked");
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on ${port}`);
+})
